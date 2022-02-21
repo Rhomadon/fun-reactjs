7ce8e6e14a7a09a4c7d8166c2226dd72f031fb57
Rhomadon <adonrhomadon100@gmail.com>
2022-02-21 20:22:35 +0700

    Membuat Portal Berita dengan Hooks

---
+++ src/FunComp/Counter.jsx
@@ -0,0 +1,60 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
+const Counter = () => {
+    let [text, setText] = useState('')
+    let [view, setView] = useState({})
+
+    useEffect(() => {
+
+        const url = 'https://newsapi.org/v2/top-headlines?apiKey=54da94e5bdee433caf334ae91c37f642&country=id&q='
+
+
+        axios.get(url+text).then(res => {
+        const article = res.data.articles
+        setView(article)
+
+            console.log(article)
+        
+        })
+
+        localStorage.setItem('text', text)
+        console.log('text');
+
+    }, [text])
+    
+
+    return (
+        <div>
+            <nav class="navbar fixed-top navbar-light bg-light">
+                <div class="container">
+                    <h1 class="navbar-brand">RNews</h1>
+                    <form class="d-flex">
+                    <input type="text" class="form-control me-2" placeholder="Search" aria-label="Search" onChange={(a) => setText(a.target.value)}/>
+                    </form>
+                </div>
+            </nav>
+            <div class="container">
+                <div class="row row-cols-1 row-cols-md-4 g-4"> 
+                    <div class="col">
+                        <div class="card">
+                            <img src="" class="card-img-top" alt="" />
+                            <div class="card-body">
+                                <h5 class="card-title">{
+                                view.title
+                                }</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Autor</h6>
+                                <p class="card-text"><small class="text-muted">21 Januari 2022</small></p>
+                                <p class="card-text">Deskripsi</p>
+                                <button href="" class="btn btn-primary" >Selengkapnya</button>           
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Counter
---
+++ src/FunComp/News.jsx
@@ -0,0 +1,11 @@
+import Counter from "./Counter"
+
+const News = () => {
+    return (
+        <div>
+            <Counter />
+        </div>
+    )
+}
+
+export default News
